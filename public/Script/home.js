dfa99bf5e3db67902aa5d3166be495ab79a6999e
Yaakov Moncharsh <coby98765@gmail.com>
2023-08-27 16:29:41 +0300

    form to sheet completed

--- public/Script/home.js
+++ public/Script/home.js
@@ -2,23 +2,12 @@ const form = document.getElementById('sheetdb-form');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    let formData = new FormData(document.getElementById('sheetdb-form'))
-    let name = formData.get('data[name]')
-    let email = formData.get('data[email]')
-    let message = formData.get('data[message]')
-    console.log(name, email, message);
     fetch(form.action, {
         method: 'POST',
-        body: {
-            name,
-            email,
-            message,
-        }
+        body: new FormData(form),
     }).then(
         response => response.json()
     ).then((html) => {
         alert("Message Sent!");
-    }).catch((html) => {
-        alert("Message Not Sent!");
-    });
+    })
 });
