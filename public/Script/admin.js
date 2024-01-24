062bb44e543f972805bf1e471c5cb11caf459ff7
yaakov98765 <108609065+yaakov98765@users.noreply.github.com>
2024-01-24 17:15:36 +0200

    admin panel

--- public/Script/admin.js
+++ public/Script/admin.js
@@ -1,141 +1,192 @@
+//SideNav Mobile Settings
+function toggleNav() {
+    const sideNav = document.querySelector('.sideNav');
+    const overlay = document.querySelector('.overlay');
+    const closeButton = document.querySelector('.close-button');
 
-document.addEventListener("DOMContentLoaded", table_4knr9i_load_content);
-var table_4knr9i_data = null;
-
-window.table_4knr9i_load_content = table_4knr9i_load_content;
-
-function table_4knr9i_load_content(e, visableColumns = null, searchableColumns = null) {
-    let content = document.getElementById("table_4knr9i__sheetdb_content");
-    if (!content) {
-        return null;
-    }
-
-    let appContent = document.getElementById("table_4knr9i__table_content");
-    let loader = document.getElementById("table_4knr9i__loader");
-    let apiUrl = content.dataset.apiUrl;
-    let errorNode = document.getElementById("table_4knr9i_snippet_error");
-    
-    appContent.style.display = 'none';
-    
-    loader.style.display = 'block';
-    if (!visableColumns) {
-        visableColumns = JSON.parse('[\"name\",\"email\",\"message\"]');
-    } if (!searchableColumns) {
-        searchableColumns = JSON.parse('[\"name\",\"email\",\"message\"]');
-    } if (table_4knr9i_data) {
-        table_4knr9i_data.forEach((row) => {
-            let rowNode = document.createElement('tr');
-            visableColumns.forEach((column) => {
-                let colNode = document.createElement('td');
-                colNode.innerHTML = row[column];
-                colNode.dataset.label = column;
-                colNode.dataset.searchable = searchableColumns.includes(column) ? "true" : "false";
-                rowNode.appendChild(colNode);
-            });
-            content.appendChild(rowNode);
-            appContent.style.display = '';
-            loader.style.display = 'none';
-        }); return;
-    } if (apiUrl) {
-        content.innerHTML = '';
-        fetch(apiUrl).then((response) => {
-            if (response.status !== 200) {
-                loader.style.display = 'none';
-                errorNode.style.display = 'flex';
-                return false;
-            } return response.json();
-        }).then((data) => {
-            if (!data) {
-                loader.style.display = 'none';
-                errorNode.style.display = 'flex';
-            }
-            table_4knr9i_data = data; data.forEach((row) => {
-                let rowNode = document.createElement('tr');
-                visableColumns.forEach((column) => {
-                    let colNode = document.createElement('td');
-                    colNode.innerHTML = row[column];
-                    colNode.dataset.label = column;
-                    colNode.dataset.searchable = searchableColumns.includes(column) ? "true" : "false";
-                    rowNode.appendChild(colNode);
-                });
-                content.appendChild(rowNode);
-                appContent.style.display = '';
-                loader.style.display = 'none';
-            });
-        });
-    }
-}
-window.table_4knr9i_sort_table = table_4knr9i_sort_table;
-function table_4knr9i_sort_table(n) {
-    let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-    table = document.getElementById("table_4knr9i__table");
-    switching = true;
-    dir = "asc"; while (switching) {
-        switching = false;
-        rows = table.rows;
-        for (i = 1; i < (rows.length - 1); i++) {
-            shouldSwitch = false; x = rows[i].getElementsByTagName("TD")[n];
-            y = rows[i + 1].getElementsByTagName("TD")[n];
-            if (!x || !y) {
-                continue;
-            }
-            let xVal = parseInt(x.innerHTML.toLowerCase());
-            let yVal = parseInt(y.innerHTML.toLowerCase());
-            if (isNaN(xVal)) {
-                xVal = x.innerHTML.toLowerCase();
-            }
-            if (isNaN(yVal)) {
-                yVal = y.innerHTML.toLowerCase();
-            }
-            if (dir == "asc") {
-                if (xVal > yVal) {
-                    shouldSwitch = true;
-                    break;
-                }
-            } else if (dir == "desc") {
-                if (xVal < yVal) {
-                    shouldSwitch = true;
-                    break;
-                }
-            }
-        } if (shouldSwitch) {
-            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-            switching = true;
-            switchcount++;
-        } else {
-            if (switchcount == 0 && dir == "asc") {
-                dir = "desc";
-                switching = true;
-            }
-        }
-    }
-}
-window.table_4knr9i_form_search_change = table_4knr9i_form_search_change;
-function table_4knr9i_form_search_change() {
-    var input, filter, table, tr, td, cell, i, j, count, no_results;
-    nput = document.getElementById("table_4knr9i_form_search");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("table_4knr9i__table");
-    tr = table.getElementsByTagName("tr");
-    no_results = document.getElementById("table_4knr9i__no_results_found");
-    count = 0;
-    for (i = 1; i < (tr.length - 1); i++) {
-        tr[i].style.display = "none";
-        td = tr[i].getElementsByTagName("td");
-        for (var j = 0; j < td.length; j++) {
-            if (td[j].dataset.searchable == "false") {
-                continue;
-            }
-            cell = tr[i].getElementsByTagName("td")[j]; if (cell) {
-                if (cell.innerText.toUpperCase().indexOf(filter) > -1) {
-                    tr[i].style.display = "";
-                    count++; break;
-                }
-            }
-        }
-    } if (count === 0) {
-        no_results.style.display = "table - row";
+    sideNav.classList.toggle('open');
+    if (sideNav.style.left === '0px') {
+        sideNav.style.left = '-300px';
+        overlay.style.display = 'none';
+        closeButton.style.display = 'none';
     } else {
-        no_results.style.display = "none";
+        sideNav.style.left = '0px';
+        overlay.style.display = 'block';
+        closeButton.style.display = 'block';
     }
 }
+
+// select class toggle
+function selectButton(button) {
+    // Remove 'selected' class from all buttons
+    var buttons = document.querySelectorAll('.sideNav button');
+    buttons.forEach(function (btn) {
+        btn.classList.remove('selected');
+    });
+
+    // Add 'selected' class to the clicked button
+    button.classList.add('selected');
+
+    // Show/hide the infoBoxMessages div based on the selected button
+    var infoBoxMessages = document.getElementById('infoBoxMessages');
+    if (button.textContent === 'Messages' && button.classList.contains('selected')) {
+        infoBoxMessages.style.display = 'block';
+    } else {
+        infoBoxMessages.style.display = 'none';
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const dataChartBody = document.getElementById('dataMessagesChartBody');
+    const messagesPerPage = 15;
+    let currentPage = 1;
+
+    // Fetch data from the API endpoint
+    fetch('https://sheetdb.io/api/v1/2zmnvwg2pldlh')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Display initial set of messages
+            displayMessages(data, currentPage, messagesPerPage);
+
+            // Add event listeners to buttons
+            const prevButton = document.getElementById('prevButton');
+            const nextButton = document.getElementById('nextButton');
+
+            prevButton.addEventListener('click', function () {
+                if (currentPage > 1) {
+                    currentPage--;
+                    displayMessages(data, currentPage, messagesPerPage);
+                }
+            });
+
+            nextButton.addEventListener('click', function () {
+                const totalPages = Math.ceil(data.length / messagesPerPage);
+                if (currentPage < totalPages) {
+                    currentPage++;
+                    displayMessages(data, currentPage, messagesPerPage);
+                }
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            // Display an error message on the page
+            const errorMessage = document.createElement('div');
+            errorMessage.textContent = 'Error fetching data. Please try again later.';
+            document.body.appendChild(errorMessage);
+        });
+
+    function createNavigationButton(label, onClick) {
+        const button = document.createElement('button');
+        button.textContent = label;
+        button.addEventListener('click', onClick);
+        return button;
+    }
+
+    function createDataRow(item) {
+        const row = document.createElement('tr');
+
+        // Add columns
+        const columns = ['id', 'name', 'email', 'message', 'datetime'];
+        columns.forEach(column => {
+            const cell = document.createElement('td');
+
+            // Convert 'datetime' to local time
+            if (column === 'datetime') {
+                const localDateTime = new Date(item[column]).toLocaleString();
+                cell.textContent = localDateTime;
+            } else {
+                cell.textContent = item[column];
+            }
+
+            row.appendChild(cell);
+        });
+
+        // Add "Reply" button
+        const replyButtonCell = document.createElement('td');
+        const replyButton = document.createElement('button');
+        replyButton.innerHTML = '<i class="fa-solid fa-reply"></i>';
+        replyButton.classList.add('reply-button');
+        replyButton.addEventListener('click', function () {
+            replyToEmail(item.email,item.name);
+        });
+        replyButtonCell.appendChild(replyButton);
+        row.appendChild(replyButtonCell);
+
+        // Add delete button
+        const deleteButtonCell = document.createElement('td');
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+        deleteButton.classList.add('delete-button');
+        deleteButton.addEventListener('click', function () {
+            deleteItem(item.id);
+        });
+        deleteButtonCell.appendChild(deleteButton);
+        row.appendChild(deleteButtonCell);
+
+        return row;
+    }
+
+    function displayMessages(data, page, messagesPerPage) {
+        const startIndex = (page - 1) * messagesPerPage;
+        const endIndex = startIndex + messagesPerPage;
+        const currentPageData = data.slice(startIndex, endIndex);
+
+        // Clear existing rows
+        dataChartBody.innerHTML = '';
+
+        // Populate the table with data for the current page
+        currentPageData.forEach(item => {
+            const row = createDataRow(item);
+            dataChartBody.appendChild(row);
+        });
+
+        // Update the counter
+        const counter = document.getElementById('counter');
+        updateCounter(counter, page, data.length);
+    }
+
+    function replyToEmail(email, name) {
+        const subject = encodeURIComponent('Message from yaakov.store');
+        const to = encodeURIComponent(email);
+        const body = encodeURIComponent(`Hi ${name},`);
+
+        const gmailLink = `https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&su=${subject}&to=${to}&body=${body}`;
+
+        // Open the link in a new tab
+        window.open(gmailLink, '_blank');
+    }
+
+    function updateCounter(counterElement, currentPage, totalItems) {
+        const totalPages = Math.ceil(totalItems / messagesPerPage);
+        const startItem = (currentPage - 1) * messagesPerPage + 1;
+        const endItem = Math.min(currentPage * messagesPerPage, totalItems);
+
+        counterElement.textContent = `${startItem} - ${endItem} of ${totalItems}`;
+    }
+
+    function deleteItem(itemId) {
+        // Send DELETE request to the API endpoint
+        fetch(`https://sheetdb.io/api/v1/2zmnvwg2pldlh/id/${itemId}`, {
+            method: 'DELETE',
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Item deleted successfully:', data);
+                // Refresh the table after deletion
+                return fetch('https://sheetdb.io/api/v1/2zmnvwg2pldlh');
+            })
+            .then(response => response.json())
+            .then(newData => {
+                displayMessages(newData, currentPage, messagesPerPage);
+            })
+            .catch(error => {
+                console.error('Error deleting item:', error);
+            });
+    }
+});
